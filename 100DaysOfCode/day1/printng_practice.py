911bbac93d7b205066300f7ce4cf7627915ce58b
nmerynda <nmateuszuk@gmail.com>
2024-09-04 03:12:25 +0000

    day1 of 100 days: printing inputs band name execercise

---
+++ 100DaysOfCode/day1/printng_practice.py
@@ -0,0 +1,35 @@
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+print("Hello " + "Angela!\n")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+
+# print("Hello " + input("What is your name? ") + "!")
+
+name = input("What is your name? ")
+print(name)
+
+name = "Angela"
+print(name)
+
+length = len(name)
+print(length)
+
+
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
---
+++ 100DaysOfCode/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+# from palindrome import Solution
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    # print(Solution().isPalindrome(1236666666))
+    # print(Solution().isPalindrome(454))
+    # print(Solution().isPalindrome(5555555))
+    # print(Solution().isPalindrome(3434343443))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
