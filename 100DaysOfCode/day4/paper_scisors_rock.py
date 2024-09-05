0c4c44c24cd4268318128d104be8b15d0c3a4d35
nmerynda <nmateuszuk@gmail.com>
2024-09-05 02:32:17 +0000

    rock paper scissors

--- 100DaysOfCode/day4/paper_scisors_rock.py
+++ 100DaysOfCode/day4/paper_scisors_rock.py
@@ -1,6 +1,58 @@
 import random
 
-user_choice = (int)(
-    input("What do you choose\n? Type 0 for Rock, 1 for Paper or 2 for scissors\n"))
+rock = '''  
+    _______
+---'   ____)  
+      (_____)  
+      (_____)  
+      (____)
+---.__(___)  
+'''
 
-computer_choice = random.randint(0, 1)
+paper = '''  
+    _______
+---'   ____)____  
+          ______)  
+          _______)  
+         _______)
+---.__________)  
+'''
+
+scissors = '''  
+    _______
+---'   ____)____  
+          ______)  
+       __________)  
+      (____)
+---.__(___)  
+'''
+
+user_choice = (int)(
+    input("What do you choose? \nType 0 for Rock, 1 for Paper or 2 for scissors\n"))
+
+if 0 < user_choice < 3:
+
+    computer_choice = random.randint(0, 2)
+
+    game_images = [rock, paper, scissors]
+
+    print("User Choice: ")
+
+    print(game_images[user_choice])
+
+    print("Computer Choice: ")
+
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win! 🎉")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw.")
+else:
+    print("You typed an invalid number, You Lose, Computer wins")
