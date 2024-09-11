281a3ba11104de8b8e6b944c3f5a7d9073de4209
nmerynda <nmateuszuk@gmail.com>
2024-09-11 02:45:12 +0000

    day15 start of excercise

--- 100DaysOfCode/day14/higher_or_lower_game.py
+++ 100DaysOfCode/day14/higher_or_lower_game.py
@@ -54,3 +54,13 @@ def game():
 
 
 game()
+
+# alternative
+# def check_answer(guess, a_followers, b_followers):
+#     """Checks followers against user's guess
+#     and returns True if they got it right.
+#     Or False if they got it wrong."""
+#     if a_followers > b_followers:
+#         return guess == "a"
+#     else:
+#         return guess == "b"
---
+++ 100DaysOfCode/day15/coffee_data.py
@@ -0,0 +1,23 @@
+data = [
+    {
+        'name': 'Espresso',
+        'water': 50,
+        'coffee': 18,
+        'milk': 0,
+        'price': 1.5
+    },
+    {
+        'name': 'Latte',
+        'water': 200,
+        'coffee': 24,
+        'milk': 150,
+        'price': 2.5
+    },
+    {
+        'name': 'Cappuccino',
+        'water': 250,
+        'coffee': 24,
+        'milk': 100,
+        'price': 3
+    },
+]
