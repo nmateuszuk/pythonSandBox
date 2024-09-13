93084fe1a16bd11730584c40a8d100c244401ee0
nmerynda <nmateuszuk@gmail.com>
2024-09-13 02:44:24 +0000

    coffee nachine improvements

--- 100DaysOfCode/day15/coffee_machine.py
+++ 100DaysOfCode/day15/coffee_machine.py
@@ -1,4 +1,3 @@
-import random
 from coffee_data import data
 
 initial_state = {
@@ -14,15 +13,15 @@ def return_count(quarters, dimes, nickels, pennies, price):
     given += quarters * 0.25 + dimes * 0.1 + nickels * 0.05 + pennies * 0.01
     if given < price:
         print("Sorry that's not enough money. Money refunded")
-        return -price
-    return price-given
+        return -given
+    return given-price
 
 
 def if_sufficient(drink):
     dict = {}
-    dict["water"] = drink["water"] < initial_state["water"]
-    dict["milk"] = drink["milk"] < initial_state["milk"]
-    dict["coffee"] = drink["coffee"] < initial_state["coffee"]
+    dict["water"] = drink["water"] <= initial_state["water"]
+    dict["milk"] = drink["milk"] <= initial_state["milk"]
+    dict["coffee"] = drink["coffee"] <= initial_state["coffee"]
 
 # Test Boolean Value of Dictionary
 # Using list comprehension
@@ -49,5 +48,28 @@ while pick != "off":
     if pick == "report":
         print(initial_state)
         continue
+    if pick not in data:
+        print("Invalid selection. Please try again.")
+        continue
 
-    if_sufficient(data[pick])
+    if if_sufficient(data[pick]):
+        price = data[pick]["price"]
+        print(f"{pick} costs ${price:.2f}")
+        quarters = int(input("How many quarters?: "))
+        dimes = int(input("How many dimes?: "))
+        nickels = int(input("How many nickels?: "))
+        pennies = int(input("How many pennies?: "))
+        change = return_count(quarters, dimes, nickels, pennies, price)
+
+        if change >= 0:
+            print(f"Here is ${change:.2f} in change.")
+            print(f"Here is your {pick}. Enjoy!")
+            initial_state['water'] -= data[pick]['water']
+            initial_state['milk'] -= data[pick]['milk']
+            initial_state['coffee'] -= data[pick]['coffee']
+            initial_state['money'] += price
+        else:
+            print(f"Return {-change}. Pick another drink")
+    else:
+        print("Pick another drink")
+        continue
