8b27dda32ce606a202bcf3fdf6c7b7141fc38aa6
natalia merynda <n.mateuszuk@gmail.com>
2024-10-09 02:44:55 +0000

    day 21- inheritance, slicing, finished snake

---
+++ 100DaysOfCode/day20 & day21/slicing.py
@@ -0,0 +1,11 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+print(piano_tuple[1:])
+print(piano_tuple[2:5])
+
+# every second one
+print(piano_tuple[::2])
+
+# reverse
+print(piano_tuple[::-1])
